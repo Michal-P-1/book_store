4493153c418f561467716f79b10630918118bfd2
Michal Podolak <60146710+myky123@users.noreply.github.com>
2024-05-21 21:33:53 +0100

    Contact form functionality

--- js/contactForm.js
+++ js/contactForm.js
@@ -2,6 +2,21 @@ const contactForm = document.querySelector(".contact-form");
 const contactFormButton = document.querySelector(".contact-form-btn");
 const contactFormInputs = contactForm.querySelectorAll(".form-control");
 
-contactFormButton.addEventListener("click", () => {
-    contactForm.reset();
+contactFormButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    let error = false;
+    contactFormInputs.forEach((input) => {
+        if (input.value === "") {
+            error = true;
+        }
+    });
+
+    if (!error) {
+        alert(
+            "Thank you for your message! We will get back to you as soon as possible."
+        );
+        contactForm.submit();
+    } else {
+        alert("Please fill in all fields.");
+    }
 });
